5e09bbec573a32bbb7e4e0436c7789d9d194506c
samahthwib <samah.thweib@hotmail.com>
2020-04-08 14:43:17 +0300

    finished the features

--- schema.sql
+++ schema.sql
@@ -1,6 +1,4 @@
 DROP TABLE IF EXISTS locations;
--- DROP TABLE IF EXISTS weather;
--- DROP TABLE IF EXISTS trails;
 
 CREATE TABLE locations(
   id SERIAL PRIMARY KEY,
@@ -10,18 +8,3 @@ CREATE TABLE locations(
   longitude DECIMAL(9,7)
 );
 
--- CREATE TABLE weather(
---   id SERIAL PRIMARY KEY,
---   search_query VARCHAR(255),
---   formatted_query VARCHAR(255),
---   latitude DECIMAL(9,7),
---   longitude DECIMAL(9,7)
--- );
-
--- CREATE TABLE trails(
---   id SERIAL PRIMARY KEY,
---   search_query VARCHAR(255),
---   formatted_query VARCHAR(255),
---   latitude DECIMAL(9,7),
---   longitude DECIMAL(9,7)
--- );
